18624d3ab7b75f1dd4ca4e11617982951c74e553
doodoodoong <doo_doong@hanmail.net>
2024-10-02 08:56:36 +0900

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AI 학습 도우미",
+  title: "증도GPT Ver.2",
   description: "증도초등학교 학생을 위한 AI 채팅 서비스",
 };
 
