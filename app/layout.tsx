30cbe49ca8e68962bf8c434723a781169eeb127f
doodoodoong <109060047+doodoodoong@users.noreply.github.com>
2025-03-03 21:15:17 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "증도GPT Ver.2",
-  description: "증도초등학교 학생을 위한 AI 채팅 서비스",
+  title: "장산GPT",
+  description: "장산초등학교 학생을 위한 AI 채팅 서비스",
 };
 
 export default function RootLayout({
