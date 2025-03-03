3361400430f8d69de74e1f8a07e7c946c5c9f3e0
doodoodoong <109060047+doodoodoong@users.noreply.github.com>
2025-03-03 21:19:30 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "장산GPT",
   description: "장산초등학교 학생을 위한 AI 채팅 서비스",
+  icon: "/jangsan.png"
 };
 
 export default function RootLayout({
